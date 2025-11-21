2e10b3882933e130c324ea169dc2155d1d3c0265
DarkGrimm10 <darkar132xd@gmail.com>
2025-11-21 01:01:07 +0000

    Enhance theme support by adding light and dark color variables, shared colors, and border radius in Tailwind configuration; update dashboard view with primary button

--- laravel-project/resources/views/dashboard.blade.php
+++ laravel-project/resources/views/dashboard.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
+        <button type="button" class="whitespace-nowrap rounded-radius bg-primary border border-primary px-4 py-2 text-sm font-medium tracking-wide text-on-primary transition hover:opacity-75 text-center focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 disabled:opacity-75 disabled:cursor-not-allowed dark:bg-primary-dark dark:border-primary-dark dark:text-on-primary-dark dark:focus-visible:outline-primary-dark">Primary</button>
+
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
